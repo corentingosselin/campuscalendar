5de3cbe5f316ad889224a5352e5485e71b2dc562
prodigy <coco_gigpn@hotmail.com>
2023-11-08 11:34:12 +0100

    deploy

--- tools/scripts/build-push-docker.sh
+++ tools/scripts/build-push-docker.sh
@@ -2,6 +2,9 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
+# Define the base name for your docker images
+REGISTRY_BASE="registry.prodigyman.fr:5000"
+
 # Declare an associative array of services and their Dockerfile locations
 declare -A services=(
   ["api-gateway"]="apps/api-gateway/docker/Dockerfile.prod"
@@ -15,25 +18,32 @@ declare -A services=(
 build_image() {
   local service=$1
   local dockerfile=$2
-  echo "Building $service..."
-  docker build -t "campuscalendar/$service:latest" -f $dockerfile "apps/$service/"
-  echo "$service built successfully."
+  local version_tag=$3
+  local image_name="${REGISTRY_BASE}/${service}:${version_tag}"
+  echo "Building ${image_name}..."
+  docker build -t "${image_name}" -f $dockerfile "apps/$service/"
+  echo "${service} built successfully."
 }
 
 # Function to push a Docker image
 push_image() {
   local service=$1
-  echo "Pushing $service to registry..."
-  docker push "campuscalendar/$service:latest"
-  echo "$service pushed successfully."
+  local version_tag=$2
+  local image_name="${REGISTRY_BASE}/${service}:${version_tag}"
+  echo "Pushing ${image_name} to registry..."
+  docker push "${image_name}"
+  echo "${service} pushed successfully."
 }
+
+# The version or tag for the images, could be set dynamically or passed into the script
+VERSION_TAG="latest"
 
 # Build all Docker images
 for service in "${!services[@]}"; do
-  build_image "$service" "${services[$service]}"
+  build_image "$service" "${services[$service]}" "$VERSION_TAG"
 done
 
 # Push all Docker images
 for service in "${!services[@]}"; do
-  push_image "$service"
+  push_image "$service" "$VERSION_TAG"
 done
