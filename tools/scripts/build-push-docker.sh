12210e409d04c74533d0425536d6a7b7d119a4cd
atzisb-kozak <alkergoat@gmail.com>
2023-11-08 14:24:48 +0000

    [AKT] debug CI build action + update script deploy

--- tools/scripts/build-push-docker.sh
+++ tools/scripts/build-push-docker.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Define the base name for your docker images
-REGISTRY_BASE="registry.prodigyman.fr"
+REGISTRY_BASE="gcr.io/campuscalendar"
 
 # Declare an associative array of services and their Dockerfile locations
 declare -A services=(
