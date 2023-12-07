f4da4b08c7925a19c719beedd94df97c78ef9081
prodigy <coco_gigpn@hotmail.com>
2023-12-07 15:09:07 +0100

    fix env userservice

--- libs/backend/user-service/core/src/lib/user-service-core.module.ts
+++ libs/backend/user-service/core/src/lib/user-service-core.module.ts
@@ -3,6 +3,8 @@ import { MikroOrmModule } from '@mikro-orm/nestjs';
 
 import { UserServiceFeatureModule } from '@campuscalendar/backend/user-service/feature';
 import mikroOrmConfig from '../mikro-orm.config';
+import { AdminEntity } from '@campuscalendar/backend/user-service/data-access';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [],
@@ -10,15 +12,26 @@ import mikroOrmConfig from '../mikro-orm.config';
   exports: [],
   imports: [
     UserServiceFeatureModule,
-    MikroOrmModule.forRootAsync(
-      {
-        useFactory: () => ({
+    MikroOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => {
+        const dbPort = configService.get<string>('USER_SERVICE_DB_PORT');
+        return {
           registerRequestContext: false,
           debug: true,
-          ...mikroOrmConfig
-        }),
-        inject: [],
+          type: 'mysql' as const,
+          host: configService.get<string>('USER_SERVICE_MYSQL_HOST'),
+          port: dbPort ? parseInt(dbPort) : undefined,
+          dbName: configService.get<string>('MYSQL_DATABASE'),
+          user: configService.get<string>('MYSQL_USER'),
+          password: configService.get<string>('MYSQL_PASSWORD'),
+          entities: [AdminEntity],
+          migrations: {
+            path: 'apps/user-service/migrations',
+          },
+        };
+      },
 
+      inject: [ConfigService],
     }),
   ],
 })
