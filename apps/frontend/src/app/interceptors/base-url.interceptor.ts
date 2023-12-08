8e039c9bb2edf0b81c583e157fb7c9a4ecbd469f
prodigy <coco_gigpn@hotmail.com>
2023-12-08 14:49:00 +0100

    lol

--- apps/frontend/src/app/interceptors/base-url.interceptor.ts
+++ apps/frontend/src/app/interceptors/base-url.interceptor.ts
@@ -3,6 +3,6 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const baseUrlInterceptor: HttpInterceptorFn = (req, next) => {
   if (!environment.production) return next(req);
-  const apiReq = req.clone({ url: `${environment.apiBaseUrl}/${req.url}` });
+  const apiReq = req.clone({ url: `${environment.apiBaseUrl}${req.url}` });
   return next(apiReq);
 };
--- libs/backend/shared/network/src/lib/rpc.service.ts
+++ libs/backend/shared/network/src/lib/rpc.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
-import { catchError, lastValueFrom, throwError, timeout } from 'rxjs';
+import { catchError, lastValueFrom, tap, throwError, timeout } from 'rxjs';
 
 
-const TIMEOUT_DURATION = 5000; // 5 seconds
+const TIMEOUT_DURATION = 10000; // 5 seconds
 
 @Injectable()
 export class RpcService {
@@ -17,8 +17,15 @@ export class RpcService {
     command: string,
     payload?: unknown
   ): Promise<T> {
+    console.log('payload ', payload);
     // send 0 data if not payload, small trick to allow passing empty payload
     const result = lastValueFrom(this.client.send(command, payload || 0).pipe(
+      tap((data) => {
+        console.log('data ', data);
+        if (data instanceof RpcException) {
+          console.log('data ex ', data);
+        }
+      }),
       timeout(TIMEOUT_DURATION),
       catchError((error) => {
         return throwError(() => new RpcException(error));
